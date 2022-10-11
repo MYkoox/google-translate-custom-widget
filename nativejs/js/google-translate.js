da8fadbd674ee409a07b79d1438d1c2fccba19e7
MYkoox <115556549+MYkoox@users.noreply.github.com>
2022-10-11 20:25:08 +0300

    Update google-translate.js

--- nativejs/js/google-translate.js
+++ nativejs/js/google-translate.js
@@ -6,10 +6,10 @@
 
 const googleTranslateConfig = {
     /* Original language */
-    lang: "ru",
+    lang: "en",
     /* The language we translate into on the first visit*/
     /* Язык, на который переводим при первом посещении */
-    // langFirstVisit: 'en',
+    // langFirstVisit: 'ru',
     /* Если скрипт не работает на поддомене, 
     раскомментируйте и
     укажите основной домен в свойстве domain */
